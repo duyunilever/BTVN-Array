4f780eccc016010098e810345813bdee8a09bfca
duyunilever <nguyentienduy.uce@gmail.com>
2023-03-30 12:19:36 +0700

    fix btvn
    thêm điều kiện kiểm tra giá trị kích thước mảng nhập vào

--- BT2/src/Main.java
+++ BT2/src/Main.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nhập số lượng phần tử của mảng: ");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n;
+        System.out.println("Nhập số lượng phần tử mảng:");
+        do {
+            n = sc.nextInt();
+            if (n>0)
+                break;
+            System.out.println("Nhập sai, nhập lại số lượng phần tử mảng:");
+        } while (n <= 0);
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) {
             System.out.println("Nhập phần tử thứ " + (i + 1));
--- BT3/src/Main.java
+++ BT3/src/Main.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nhập số lượng phần tử mảng: ");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int [] arr = new int[a];
+        int a;
+        System.out.println("Nhập số lượng phần tử mảng:");
+        do {
+            a = sc.nextInt();
+            if (a>0)
+                break;
+            System.out.println("Nhập sai, nhập lại số lượng phần tử mảng:");
+        } while (a <= 0);
+
+        int[] arr = new int[a];
         for (int i = 0; i < arr.length; i++) {
-            System.out.println("Nhập phần tử thứ " + (i+1));
+            System.out.println("Nhập phần tử thứ " + (i + 1));
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
-        System.out.println("Giá trị lớn nhất trong mảng là: " + arr[arr.length-1]);
+        System.out.println("Giá trị lớn nhất trong mảng là: " + arr[arr.length - 1]);
         System.out.println("Giá trị nhỏ nhất trong mảng là: " + arr[0]);
     }
 }
